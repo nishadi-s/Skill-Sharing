d2484881c81a75d6d53b830432cd9000a66656f2
nishadi-s <IT22542038@my.sliit.lk>
2025-05-09 13:00:44 +0530

    Create user repository

---
+++ app/src/main/java/com/example/app/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.app.repositories;
+
+import com.example.app.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByProviderAndProviderId(String provider, String providerId);
+}
