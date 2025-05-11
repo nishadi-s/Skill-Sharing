e5c9b14d66c198db994b6d6549c1595f5d2bdacc
nishadi-s <IT22542038@my.sliit.lk>
2025-05-11 08:13:32 +0530

    Modifying Comments Alerts

--- client/src/components/Posts/PostDetails.jsx
+++ client/src/components/Posts/PostDetails.jsx
@@ -193,7 +193,7 @@ const PostDetails = () => {
       showCancelButton: true,
       confirmButtonColor: "#d33",
       cancelButtonColor: "#3085d6",
-      confirmButtonText: "Yes, delete it!",
+      confirmButtonText: "Yes, delete it!"
     });
 
     if (result.isConfirmed) {
